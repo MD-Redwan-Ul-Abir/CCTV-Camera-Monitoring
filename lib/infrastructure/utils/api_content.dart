21b93c08adb21d184d81e525352e3887ac33a079
MD Redwan Ul Abir <92859706+MD-Redwan-Ul-Abir@users.noreply.github.com>
2025-07-05 18:06:08 +0600

    api entrigation stated

---
+++ lib/infrastructure/utils/api_content.dart
@@ -0,0 +1,17 @@
+class  ApiConstants{
+
+
+  static String baseUrl = "https://sheakh6731.sobhoy.com/api/v1";
+  static String imageBaseUrl = "https://radwan2000.sobhoy.com";
+
+
+
+///-------------------------------------Auth------------------------------------
+
+  static String logInUrl = '$baseUrl/auth/login';
+  static String verifyEmail = '$baseUrl/auth/verify-email';
+  static String changPassUrl = '$baseUrl/auth/change-password';
+  static String forgetPassUrl = '$baseUrl/auth/forgot-password';
+  static String resetPassUrl = '$baseUrl/auth/reset-password';
+
+}
